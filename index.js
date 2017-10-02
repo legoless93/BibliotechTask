db381baee24f962c19ce2021692aa1c19678f1e3
legoless93 <legoless93@yahoo.co.kr>
2017-10-02 22:06:26 +0100

    Added some error handling

--- index.js
+++ index.js
@@ -14,26 +14,32 @@ const requestHandler = (request, response) => {
     fs.readdir(parDir, (err, files) => {
         files.forEach((file, index) => {
             const curDir = parDir+'/'+file;
-            const out = outputDir+'/'+file.replace('.epub', '/')+'index.json';
+            const fileNo = file.replace('.epub', '');
+            const out = outputDir + '/'+ fileNo +'/index.json';
             const epub = new EPub(curDir, curDir+'/images', curDir+'/chapters');
-            
-            fs.mkdir(outputDir+'/'+file.replace('.epub', ''));
-            
+
+            fs.stat(outputDir+'/'+fileNo, (err) => {
+                if(err) {
+                    fs.mkdir(outputDir+'/'+fileNo, (err) => {
+                        return console.log(err);
+                    });
+                } else {
+                    console.log("Directory exists");
+                }
+            })
+
             epub.on("end", function(){
-                
+
                 const template = '{\n' +
                       '"title":' + ' "' + epub.metadata.title + '"\n' +
                       '"contributors":' + '["' + epub.metadata.creator + '"]\n' +
-                          '}';
-                
+                      '}';
+
                 fs.writeFile(out, template);
-                
-                console.log(epub.metadata.title);
-                console.log(out);
             });
-            
+
             epub.parse();
-            
+
         })
     })
 
